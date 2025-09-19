cb74d17ebf7a29a786bbc764da7f19fbadcb621d
Dripskl <joaovitorlacerdapm@gmail.com>
2025-09-18 23:07:44 -0300

    Update main.py

--- calculadora/main.py
+++ calculadora/main.py
@@ -1,30 +1,27 @@
-from func import *  # importa todas as funções do arquivo func.py
+from func import *
 import os
-
-os.system('cls')  # limpa a tela do terminal
-
+os.system('cls')
 while True:
-    # menu principal da calculadora
     controle = int(input('\n--------Calculadora--------\n----------------------------\n1. Acessar a calculadora \n-\n2. Acessar o historico \n-\n0. Sair\n----------------------------\nDigite: '))
-    
     if controle == 0:
         os.system('cls')
         print('Ate mais...')
-        break  # encerra o programa
+        break
     elif controle == 1:
-        calcular()  # chama a função de cálculo
+        calcular()
     elif controle == 2:
         os.system('cls')
-        # menu do histórico
         controle_historico = int(input('--------Historico--------\n----------------------------\n1. Consultar historico\n-\n2. Apagar historico\n-\n0. voltar\n----------------------------\nDigite: '))
         if controle_historico == 1:
             os.system('cls')
             print('\n--------Historico--------')
-            historico()  # mostra o histórico
+            historico()
         elif controle_historico == 2:
             os.system('cls')
-            apagarhistorico()  # apaga o histórico
-
+            apagarhistorico()
+        elif controle_historico == 0:
+            os.system('cls')
+        
 
 
 """
@@ -33,6 +30,5 @@ while True:
 ██║  ██║██████╔╝██║██████╔╝╚█████╗
 ██║  ██║██╔══██╗██║██╔═══╝──╚═══██╗
 ██████╔╝██║  ██║██║██║─────██████╔╝
-╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝─────╚═════╝→
-
+╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝─────╚═════╝
 """
