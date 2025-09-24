795430f12b358e8b29c02c977c8e04797dc651fe
Dripskl <joaovitorlacerdapm@gmail.com>
2025-09-23 23:28:38 -0300

    Update main.py

--- calculadora/main.py
+++ calculadora/main.py
@@ -1,26 +1,43 @@
 from func import *
 import os
 os.system('cls')
+
+banco = sqlite3.connect('historico.db')
+cursor = banco.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS historico (
+        numero1 REAL,
+        numero2 REAL,
+        op TEXT,
+        total REAL
+    )
+    """)
+banco.commit()
 while True:
-    controle = int(input('\n--------Calculadora--------\n----------------------------\n1. Acessar a calculadora \n-\n2. Acessar o historico \n-\n0. Sair\n----------------------------\nDigite: '))
-    if controle == 0:
-        os.system('cls')
-        print('Ate mais...')
-        break
-    elif controle == 1:
-        calcular()
-    elif controle == 2:
-        os.system('cls')
-        controle_historico = int(input('--------Historico--------\n----------------------------\n1. Consultar historico\n-\n2. Apagar historico\n-\n0. voltar\n----------------------------\nDigite: '))
-        if controle_historico == 1:
+    try:
+        controle = int(input('\n--------Calculadora--------\n----------------------------\n1. Acessar a calculadora \n-\n2. Acessar o historico \n-\n0. Sair\n----------------------------\nDigite: '))
+        if controle == 0:
             os.system('cls')
-            print('\n--------Historico--------')
-            historico()
-        elif controle_historico == 2:
+            print('Ate mais...')
+            break
+        elif controle == 1:
+            calcular()
+        elif controle == 2:
             os.system('cls')
-            apagarhistorico()
-        elif controle_historico == 0:
-            os.system('cls')
+            controle_historico = int(input('--------Historico--------\n----------------------------\n1. Consultar historico\n-\n2. Apagar historico\n-\n0. voltar\n----------------------------\nDigite: '))
+            if controle_historico == 1:
+
+                os.system('cls')
+                print('\n--------Historico--------')
+                historico()
+            elif controle_historico == 2:
+                os.system('cls')
+                apagarhistorico()
+            elif controle_historico == 0:
+                os.system('cls')
+    except ValueError:
+        print('Voce digitou errado')
         
 
 
